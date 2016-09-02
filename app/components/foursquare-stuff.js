0ed12d1a9cbfb944bfe9039f6086310bddf6f178
rubalcava <radgokart@gmail.com>
2016-09-02 14:32:43 -0700

    feat: Changed app direction, is now a pizza finder using Foursquare's API

--- app/components/foursquare-stuff.js
+++ app/components/foursquare-stuff.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
     userLat: 'loading...',
     userLong: 'loading...',
     userFormattedAddress: 'loading...',
+    pizzaPlaces: [],
     getUserLoc: function() {
         var self = this;
         self.set('lat', self.userLat);
@@ -44,29 +45,47 @@ export default Ember.Component.extend({
             dataType: 'json',
             success: function(response) {
                 var myResponse = response;
-                /* If coordinates cannot be turned into address, default back to Peet's coffee in Berkeley, CA */
+                /* If coordinates cannot be turned into address, let the user know */
                 if (myResponse.results.length < 1) {
-                    self.userLat = 37.8650555;
-                    self.set('lat', self.userLat);
-                    self.userLong = -122.2581727;
-                    self.set('long', self.userLong);
-                    self.userFormattedAddress = '2501 Telegraph Ave, Berkeley, CA 94704, USA';
+                    self.userFormattedAddress = 'Could not find formatted address';
                     self.set('address', self.userFormattedAddress);
                 }
                 else {
                     self.userFormattedAddress = myResponse.results[0].formatted_address;
                     self.set('address', self.userFormattedAddress);
                 }
-                /* Call here to ensure formatted address has been obtained first, since Zillow's API requires it */
-                self.initZillowSearch();
+                /* Call here to ensure formatted address has been obtained first */
+                self.initFS();
             },
             error: function(err) {
                 console.log(err);
             }
         });
     },
-    initZillowSearch: function() {
+    initFS: function() {
         var self = this;
+        Ember.$.ajax({
+            url: 'https://api.foursquare.com/v2/venues/search?client_id=IY4MOF0VN0HHCOSRH121TJYN1P3FTVZRNCX2RU1YNF23GRBH&client_secret=O0GFJPKBRBDYSO4M52SRJBINZLFWVF4DLPNYZ3WH5NOIYVKW&v=20130815&ll=' + self.userLat + ',' + self.userLong + '&query=pizza&limit=5',
+            dataType: 'json',
+            success: function(response) {
+                var foundPlaces = response.response.venues;
+                if (foundPlaces.length > 0) {
+                    for (var i = 0; i < foundPlaces.length; i++) {
+                        self.pizzaPlaces.push({
+                            'name': foundPlaces[i].name,
+                            'address': foundPlaces[i].location.formattedAddress[0] + ', ' + foundPlaces[i].location.formattedAddress[1],
+                            'phone': foundPlaces[i].contact.formattedPhone
+                        });
+
+                    }
+                    console.log(self.pizzaPlaces);
+                }
+
+            },
+            error: function(error) {
+
+            }
+        });
 
     },
     didInsertElement() {
--- tests/integration/components/foursquare-stuff-test.js
+++ tests/integration/components/foursquare-stuff-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('zillow-stuff', 'Integration | Component | zillow stuff', {
+moduleForComponent('foursquare-stuff', 'Integration | Component | foursquare stuff', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{zillow-stuff}}`);
+  this.render(hbs`{{foursquare-stuff}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#zillow-stuff}}
+    {{#foursquare-stuff}}
       template block text
-    {{/zillow-stuff}}
+    {{/foursquare-stuff}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
